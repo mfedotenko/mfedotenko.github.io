c1b54d836726b29931b1e89814d708b461b2d2dc
mfedotenko <mfedotenko@mail.ru>
2018-04-28 15:35:30 +0300

    2

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,17 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+<script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
 
-firebase.initializeApp({
-    'messagingSenderId': '1036791400789'
-});
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC_x4-dI06Kk4x3V6DRMM9V441d_02ULRo",
+    authDomain: "push2-9ee71.firebaseapp.com",
+    databaseURL: "https://push2-9ee71.firebaseio.com",
+    projectId: "push2-9ee71",
+    storageBucket: "push2-9ee71.appspot.com",
+    messagingSenderId: "1036791400789"
+  };
+  firebase.initializeApp(config);
+</script>
 
 firebase.messaging();
 
